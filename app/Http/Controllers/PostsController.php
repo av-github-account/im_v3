b5c317385072f1c5f88ca60ab67404eb8caeb9d8
av-github-account <av.avramenko@gmail.com>
2020-04-05 03:25:49 +0300

    test

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -10,6 +10,11 @@ class PostsController extends Controller
     {
       $post = \DB::table('posts')->where('slug', $slug)->first();
 //      dd($post);
+
+      if (! $post) {
+         abort(404, 'Извините, страница не найдена!');
+      };
+
       return view('post',[
          'post' => $post
       ]);
