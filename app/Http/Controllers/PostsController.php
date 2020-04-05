e11bcc429fb1bb0c68d008027e17f332a25cca13
av-github-account <av.avramenko@gmail.com>
2020-04-05 04:12:08 +0300

    Закончил урок 11

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,19 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use db;
+use App\Post;
+
 class PostsController extends Controller
 {
-    public function show($slug)
-    {
-      $post = \DB::table('posts')->where('slug', $slug)->first();
-//      dd($post);
-
-      if (! $post) {
-         abort(404, 'Извините, страница не найдена!');
-      };
-
-      return view('post',[
-         'post' => $post
+   public function show($slug)
+   {
+      return View('post', [
+         'post' => Post::where('slug', $slug)->firstOrFail()
       ]);
+   //   $post = Post::where('slug',$slug)->firstOrFail();
+   //   //      $post = DB::table('posts')->where('slug', $slug)->first();
+   //   //      dd($post);
+   //
+   //   //      if (! $post) {
+   //   //         abort(404, 'Извините, страница не найдена!');
+   //   //      };
+
+   //   return view('post',[
+   //      'post' => $post
+   //   ]);
    }
 }
